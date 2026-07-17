5229a28608d23dacb07844550c2a91137bfb13d5
Andrea Panattoni <apanatto@redhat.com>
2026-07-17 20:33:57 +0000

    rebuild by flavor

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo rm -rfv srv6raw/appliance/cache/* srv6raw/appliance/temp/*
+FLAVOR="${1:-}"
+
+
+sudo rm -rfv ${FLAVOR}/appliance/cache/* ${FLAVOR}/appliance/temp/*
 sudo rm -fv $(find . | grep openshift_install)
-./srv6raw/appliance/generate_appliance.sh srv6raw/pull-secret.json ~/.ssh/id_ed25519.pub
-./srv6raw/configimage/generate_config_image.sh srv6raw/pull-secret.json
+./${FLAVOR}/appliance/generate_appliance.sh ${FLAVOR}/pull-secret.json ~/.ssh/id_ed25519.pub
+./${FLAVOR}/configimage/generate_config_image.sh ${FLAVOR}/pull-secret.json
 sudo rm -f /opt/cache/hbn/*
-mv srv6raw/appliance/appliance.iso srv6raw/configimage/configimage/agentconfig.noarch.iso /opt/cache/hbn/
+mv ${FLAVOR}/appliance/appliance.iso ${FLAVOR}/configimage/configimage/agentconfig.noarch.iso /opt/cache/hbn/
 sudo restorecon -RFv /opt/cache/hbn/
 ./idrac.sh 192.168.132.150 http://192.168.132.10:8080/hbn/appliance.iso http://192.168.132.10:8080/hbn/agentconfig.noarch.iso
 ./idrac.sh 192.168.132.151 http://192.168.132.10:8080/hbn/appliance.iso http://192.168.132.10:8080/hbn/agentconfig.noarch.iso
