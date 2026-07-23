5225febea8bdefa339c99349a7dc3deb63d8b85a
Robin Jarry <robin@jarry.cc>
2026-07-23 13:58:19 +0200

    wip
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- srv6raw-grout/configimage/extras/setup-network.sh
+++ srv6raw-grout/configimage/extras/setup-network.sh
@@ -29,9 +29,6 @@ echo "Setting up network infrastructure via grout"
 
 # Assign VTEP address to underlay port to allow creation of the VXLAN interface
 grcli address add ${VTEP_IP}/32 iface underlay0
-grcli address add ${UNDERLAY_V6}/128 iface underlay0
-grcli address add ${LOOPBACK_V6}/128 iface underlay0
-grcli address add ${SRV6_SOURCE}/128 iface underlay0
 
 # Create VRF, bridge, VXLAN (VLAN bridge port is added later by
 # bridge-refresher after zebra has learned the VNI -- FRR #21190).
